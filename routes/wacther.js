fa044421b6792fda2bcaa2b7c637ab525814dcad
IceBro <pasha1ruh@gmail.com>
2017-11-17 08:55:13 +0200

    Add request for players online

--- routes/wacther.js
+++ routes/wacther.js
@@ -1,4 +1,19 @@
 const express = require('express'),
     router = express.Router();
 
+const playerDB = require('../playersDB');
+
+router.get('/getOnlinePlayers', (req, res) => {
+    playerDB.getAll()
+        .then((players) => {
+            players = players.filter((player) => {
+                return (Date.now() - player.lastSeenOnline) * 1000 * 60 < 1.0;
+            });
+
+            players = players.map((player) => player.id);
+
+            res.status(200).send({ players });
+        })
+});
+
 module.exports = router;
